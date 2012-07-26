5075801fac66a661f2e3c8ae62e54bf03bfc498c
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-07-26 16:21:29 +0000

    included externals
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/affiliate@6723 f53fe46e-8c02-459b-9fdd-93e75771b278

--- affiliate.php
+++ affiliate.php
@@ -4,7 +4,7 @@ Plugin Name: Affiliate
 Plugin URI: http://premium.wpmudev.org/project/wordpress-mu-affiliate
 Description: This plugin adds a simple affiliate system to your site.
 Author: Barry (Incsub)
-Version: 3.0.1
+Version: 3.0.2
 Author URI: http://premium.wpmudev.org
 WDP ID: 106
 */
@@ -18,6 +18,8 @@ set_affiliate_url(__FILE__);
 set_affiliate_dir(__FILE__);
 
 if(is_admin()) {
+	include_once('affiliateincludes/external/wpmudev-dash-notification.php');
+
 	// Only include the administration side of things when we need to
 	include_once('affiliateincludes/classes/affiliateadmin.php');
 	include_once('affiliateincludes/classes/affiliatedashboard.php');
