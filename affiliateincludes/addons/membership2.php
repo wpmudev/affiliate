da7c2b5a55f15170f391b457e197b8ccf8ccd76f
Joel James <me@joelsays.com>
2017-04-06 23:19:08 +0530

    Comment changes

--- affiliateincludes/addons/membership2.php
+++ affiliateincludes/addons/membership2.php
@@ -123,7 +123,7 @@ class Affiliate_Membership2_Integration {
 		global $affiliate; // Used for communication with Affiliates plugin.
 		global $blog_id, $site_id; // Used for logging.
 
-		// Pay only if it is an active membership.
+		// Don't pay a commission if the membership is not active.
 		if ( $subscription->status != $subscription::STATUS_ACTIVE ) {
 			return;
 		}
